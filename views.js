d3873811519dba1071305ed05ae2cc25ddac2f37
Dashiell BarkHuss <dashiellbarkhuss@Dashiells-MacBook-Pro.local>
2019-08-14 18:18:24 -0500

    circle change height depending on content height

--- views.js
+++ views.js
@@ -12,11 +12,26 @@ const setTrackerHeight = function(){
     const trackers = document.getElementsByClassName('tracker');
     const width = trackers[0].offsetWidth;
     for (i = 0; i<trackers.length; i ++){
-        console.log("w: ", width + 'px');
-        trackers[i].style.height = width + 'px';
+        const bottomOfLastChild = getPosition(trackers[i].children[1]) + trackers[i].children[1].offsetHeight;
+        const topOfFirstChild = getPosition(trackers[i].children[0]);
+        const contentHeight = bottomOfLastChild-topOfFirstChild;
+        contentHeight +40 < width? trackers[i].style.height = width + 'px': trackers[i].style.height = (contentHeight +40)+'px';
         console.log('h:',trackers[i].style.height);
     }
 }
+
+//helper found online
+function getPosition(element) {
+    var yPosition = 0;
+
+    while(element) {
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return yPosition;
+}
+
 
 
 window.onresize = setTrackerHeight;
